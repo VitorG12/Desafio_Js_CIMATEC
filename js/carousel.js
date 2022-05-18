f9c737f54258c0e377515f1aa4084c6826a9529b
Vitor Gabriel <vitorgabriel42@hotmail.com>
2022-05-18 00:20:08 +0000

    Inicialização de váriaveis e criação de métodos acessores

--- js/carousel.js
+++ js/carousel.js
@@ -4,19 +4,59 @@
 
 //Array storage class
 let carouselArr = [];
-
-
 //class Carousel
-class Carousel {
+export class Carousel {
+    //static properties
+    static _size = 0;
+    static _sequence = 0;
+    static _interval = 1;
+    //constructor
+    constructor(img, desc, link){
+        this._img = img;
+        this.desc = desc;
+        this.linkImg = link;
+    }
+    //accessor method
+    setImg(img){
+        this._img = img;
+    }
 
-    
-      
+    getImg(){
+        return this._img;
+    }
+
+    setSize(size){
+        this._size = size;
+    }
+
+    getSize(){
+        return this._size;
+    }
+
+    setInterval(sec){
+        this._interval = sec;
+    }
+
+    getInterval(){
+        return this._interval;
+    }
+
+    setSequence(num){
+        this._sequence = num;
+    }
+
+    getSequence(){
+        return this._sequence;
+    }
+
+
+
     static Start(arr){
         if(arr){
 
             if(arr.length > 0){
-                Carousel._sequence = 0;
-                Carousel._size = arr.length;
+                Carousel.setSequence(0);
+                Carousel.setSize(arr.length);
                 Carousel.Next(); //start
                 Carousel._interval = setInterval(function(){ Carousel.Next(); },5000);
             }
